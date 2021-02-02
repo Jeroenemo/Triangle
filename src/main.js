f699ec9391cd5e5493483eca27c8cae1e95e8407
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-01 20:07:33 -0800

    add jest, babel

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
+import Triangle from './triangle.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
 
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
